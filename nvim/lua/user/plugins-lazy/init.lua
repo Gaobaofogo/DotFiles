1c708e9c886264483299b24bad3f118f9e90835b
Gabriel Augusto Nunes Nóbrega Diniz <gabriel.diniz@radixeng.com>
2024-02-25 23:16:39 -0300

    feat: ajustando lsp

--- nvim/lua/user/plugins-lazy/init.lua
+++ nvim/lua/user/plugins-lazy/init.lua
@@ -1,114 +1,190 @@
 return {
-    {
-        "catppuccin/nvim",
-        as = "catppuccin",
-        config = function()
-            vim.cmd.colorscheme("catppuccin")
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        end,
-    },
-    { "nvim-treesitter/playground" },
-    {
-        "ThePrimeagen/harpoon",
-        config = function()
-            vim.keymap.set("n", "<leader>a", require("harpoon.mark").add_file)
-            vim.keymap.set("n", "<C-e>", require("harpoon.ui").toggle_quick_menu)
+	{
+		"catppuccin/nvim",
+		as = "catppuccin",
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		end,
+	},
+	{ "nvim-treesitter/playground" },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		config = function()
+			local harpoon = require("harpoon")
 
-            vim.keymap.set("n", "<C-h>", function()
-                require("harpoon.ui").nav_file(1)
-            end)
-            vim.keymap.set("n", "<C-t>", function()
-                require("harpoon.ui").nav_file(2)
-            end)
-            vim.keymap.set("n", "<C-n>", function()
-                require("harpoon.ui").nav_file(3)
-            end)
-            vim.keymap.set("n", "<C-s>", function()
-                require("harpoon.ui").nav_file(4)
-            end)
-        end,
-    },
-    {
-        "mbbill/undotree",
-        config = function()
-            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-        end,
-    },
-    {
-        "tpope/vim-fugitive",
-        config = function()
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        end,
-    },
-    {
-        "akinsho/toggleterm.nvim",
-        tag = "*",
-        config = function()
-            require("toggleterm").setup({
-                open_mapping = [[<c-\>]],
-                direction = "float",
-                hide_numbers = true,
-                shade_filetypes = {},
-                shade_terminals = true,
-                shading_factor = 2,
-                start_in_insert = true,
-                insert_mappings = true,
-                persist_size = true,
-                close_on_exit = true,
-                --shell = "powershell -NoLogo",
-                float_opts = {
-                    border = "curved",
-                    winblend = 0,
-                    highlights = {
-                        border = "Normal",
-                        background = "Normal",
-                    },
-                },
-            })
-        end,
-    },
-    { "folke/zen-mode.nvim" },
-    { "windwp/nvim-ts-autotag" },
-    {
-        "nvimdev/dashboard-nvim",
-        event = "VimEnter",
-        config = function()
-            require("dashboard").setup({
-                -- config
-            })
-        end,
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    {
-        "numToStr/Comment.nvim",
-        opts = {
-            -- add any options here
-        },
-        lazy = false,
-        config = function()
-            require("Comment").setup()
-        end,
-    },
-    {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        config = function()
-            require("ts_context_commentstring").setup({})
-        end,
-    },
-    { "nvim-lua/plenary.nvim" },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {},
-        config = function()
-            require("ibl").setup()
-        end,
-    },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function() end,
-        opts = {},
-    },
+			-- REQUIRED
+			harpoon:setup()
+			-- REQUIRED
+
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():append()
+			end)
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+
+			vim.keymap.set("n", "<C-h>", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<C-t>", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<C-n>", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<C-s>", function()
+				harpoon:list():select(4)
+			end)
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<C-S-P>", function()
+				harpoon:list():prev()
+			end)
+			vim.keymap.set("n", "<C-S-N>", function()
+				harpoon:list():next()
+			end)
+		end,
+	},
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+		config = function()
+			require("toggleterm").setup({
+				open_mapping = [[<c-\>]],
+				direction = "float",
+				hide_numbers = true,
+				shade_filetypes = {},
+				shade_terminals = true,
+				shading_factor = 2,
+				start_in_insert = true,
+				insert_mappings = true,
+				persist_size = true,
+				close_on_exit = true,
+				--shell = "powershell -NoLogo",
+				float_opts = {
+					border = "curved",
+					winblend = 0,
+					highlights = {
+						border = "Normal",
+						background = "Normal",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"folke/zen-mode.nvim",
+		config = function()
+			vim.keymap.set("n", "<leader>zz", function()
+				require("zen-mode").toggle()
+			end)
+		end,
+	},
+	{ "windwp/nvim-ts-autotag" },
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({
+				-- config
+			})
+		end,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
+		},
+		lazy = false,
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		config = function()
+			require("ts_context_commentstring").setup({})
+		end,
+	},
+	{ "nvim-lua/plenary.nvim" },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+		config = function()
+			require("ibl").setup()
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function() end,
+		opts = {},
+	},
+	{
+		-- Este pacote mascara os segredos dos .env
+		-- Ex.:
+		-- foo=secret123
+		--  ->
+		-- foo=********
+		"laytan/cloak.nvim",
+		config = function()
+			require("cloak").setup({
+				enabled = true,
+				cloak_character = "*",
+				-- The applied highlight group (colors) on the cloaking, see `:h highlight`.
+				highlight_group = "Comment",
+				-- Applies the length of the replacement characters for all matched
+				-- patterns, defaults to the length of the matched pattern.
+				cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
+				-- Whether it should try every pattern to find the best fit or stop after the first.
+				try_all_patterns = true,
+				patterns = {
+					{
+						-- Match any file starting with '.env'.
+						-- This can be a table to match multiple file patterns.
+						file_pattern = ".env*",
+						-- Match an equals sign and any character after it.
+						-- This can also be a table of patterns to cloak,
+						-- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
+						cloak_pattern = "=.+",
+						-- A function, table or string to generate the replacement.
+						-- The actual replacement will contain the 'cloak_character'
+						-- where it doesn't cover the original text.
+						-- If left empty the legacy behavior of keeping the first character is retained.
+						replace = nil,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+
+		config = function()
+			require("fidget").setup({})
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
 }
--- nvim/lua/user/plugins-lazy/telescope.lua
+++ nvim/lua/user/plugins-lazy/telescope.lua
@@ -9,5 +9,6 @@ return {
         vim.keymap.set("n", "<leader>ps", function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
+        vim.keymap.set("n", "<leader>ph", builtin.help_tags, {})
     end,
 }
